310f522074d56ff074fd6d351f9767e579f4f8e7
tk-zzz <49810315+taka-z@users.noreply.github.com>
2020-08-14 14:24:14 +0900

    Initial Commit

---
+++ prefecture.kt
@@ -0,0 +1,47 @@
+class prefecture {
+    companion object {
+        const val PREFECTURE_HOKKAIDO = 0
+        const val PREFECTURE_AOMORI = 0
+        const val PREFECTURE_IWATE = 0
+        const val PREFECTURE_MIYAGI = 0
+        const val PREFECTURE_AKITA = 0
+        const val PREFECTURE_YAMAGATA = 0
+        const val PREFECTURE_FUKUSHIMA = 0
+        const val PREFECTURE_IBARAKI = 0
+        const val PREFECTURE_TOCHIGI = 0
+        const val PREFECTURE_GUNMA = 0
+        const val PREFECTURE_SAITAMA = 0
+        const val PREFECTURE_CHIBA = 0
+        const val PREFECTURE_TOKYO = 0
+        const val PREFECTURE_KANAGAWA = 0
+        const val PREFECTURE_NIIGATA = 0
+        const val PREFECTURE_TOYAMA = 0
+        const val PREFECTURE_ISHIKAWA = 0
+        const val PREFECTURE_FUKUI = 0
+        const val PREFECTURE_YAMANASHI = 0
+        const val PREFECTURE_NAGANO = 0
+        const val PREFECTURE_GIFU = 0
+        const val PREFECTURE_SHIZUOKA = 0
+        const val PREFECTURE_AICHI = 0
+        const val PREFECTURE_MIE = 0
+        const val PREFECTURE_SHIGA = 0
+        const val PREFECTURE_KYOTO = 0
+        const val PREFECTURE_OSAKA = 0
+        const val PREFECTURE_HYOGO = 0
+        const val PREFECTURE_NARA = 0
+        const val PREFECTURE_WAKAYAMA = 0
+        const val PREFECTURE_TOTTORI = 0
+        const val PREFECTURE_SHIMANE = 0
+        const val PREFECTURE_OKAYAMA = 0
+        const val PREFECTURE_HIROSHIMA = 0
+        const val PREFECTURE_YAMAGUCHI = 0
+        const val PREFECTURE_TOKUSHIMA = 0
+        const val PREFECTURE_KAGAWA = 0
+        const val PREFECTURE_NAGASAKI = 0
+        const val PREFECTURE_KUMAMOTO = 0
+        const val PREFECTURE_OOITA = 0
+        const val PREFECTURE_MIYAZAKI = 0
+        const val PREFECTURE_KAGOSHIMA = 0
+        const val PREFECTURE_OKINAWA = 0
+    }
+}
